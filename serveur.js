3889da727e14061d8cb0c93da4e44487c2967358
olivierbranch <delalande.o@g.com>
2017-08-17 12:39:28 +0200

    Node send json

--- serveur.js
+++ serveur.js
@@ -10,9 +10,20 @@ var express = require('express');
 
 var app = express();
 
-app.get('/test', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à l\'accueil');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
 });
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).json({
+        test : 'succeedeed'
+    });
+});
+
+
 
 app.listen(8080);
