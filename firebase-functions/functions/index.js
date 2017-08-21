13b458399baadf1e8026fbf3abbc88a4388fe1af
jonathannicolas <jonathan.s.nicolas@gmail.com>
2017-08-21 18:37:56 +0200

    Add my version

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -8,6 +8,7 @@ admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+//var path = require('path');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -28,6 +29,16 @@ app.use(bodyParser.json());
         console.log('json from front', frontjson);
         res.status(200).json({
             test: 'wwww.google.com'
+        });
+    });
+
+    app.post('/upload', function (req, res) {
+        console.log(req);
+        console.log('reqBody', req.body.files);
+        //const frontjson = req.body;
+        //console.log('json from picture node', frontjson);
+        res.status(200).json({
+            test: 'wwww.picture.com'
         });
     });
 
