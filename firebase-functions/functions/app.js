6ff247ce995018bdc07b26832524a00bf50abbf9
olivierbranch <delalande.o@g.com>
2017-08-21 14:57:22 +0200

    Add : launch node trhough cloud functions

---
+++ firebase-functions/functions/app.js
@@ -0,0 +1,35 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+    app.get('/', function (req, res) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(200).send('welcome in node');
+    });
+
+    app.post('/', function (req, res) {
+        const frontjson = req.body;
+        console.log('json from front', frontjson);
+        res.status(200).json({
+            test: 'wwww.google.com'
+        });
+    });
+
+exports.api = functions.https.onRequest(app);
+
