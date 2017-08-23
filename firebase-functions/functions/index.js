1c68493213f11476700767ea27a20b7cb54e9eca
olivierbranch <delalande.o@g.com>
2017-08-23 15:34:24 +0200

    add formidable, receive form data and render json

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -8,7 +8,10 @@ admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-//var path = require('path');
+//const multer = require("multer");
+const formidable = require('formidable');
+const http = require('http');
+const util = require('util');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,6 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
     app.get('/', function (req, res) {
         res.setHeader('Content-Type', 'text/plain');
@@ -33,14 +37,60 @@ app.use(bodyParser.json());
     });
 
     app.post('/upload', function (req, res) {
-        console.log(req);
-        console.log('reqBody', req.body.files);
-        //const frontjson = req.body;
-        //console.log('json from picture node', frontjson);
-        res.status(200).json({
-            test: 'wwww.picture.com'
+
+        const form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+
+                // Check for and handle any errors here.
+
+                console.error(err.message);
+                return;
+            }
+            console.log('files', files);
+            console.log('req',req);
+            console.log('req',req.body);
+            res.status(200).json({
+                test: 'wwww.picture.com',
+                test2: files
+            });
+        //    res.writeHead(200, {'content-type': 'text/plain'});
+        //    res.write('received upload:\n\n');
+        //
+        //    // This last line responds to the form submission with a list of the parsed data and files.
+        //
+        //    res.end(util.inspect({fields: fields, files: files}));
+        //});
+        //return;
+        //
+        //console.log('picture back');
+        //console.log('req',req);
+        //console.log('req',req.body);
+        //res.status(200).json({
+        //    test: 'wwww.picture.com'
         });
     });
 
+//app.post("/upload", multer({dest: "./uploads/"}).array("logo angular.png", 12), function(req, res) {
+//    res.send(req.files);
+//});
+
 exports.api = functions.https.onRequest(app);
 
+//var form = new formidable.IncomingForm();
+//form.parse(req, function(err, fields, files) {
+//    if (err) {
+//
+//        // Check for and handle any errors here.
+//
+//        console.error(err.message);
+//        return;
+//    }
+//    res.writeHead(200, {'content-type': 'text/plain'});
+//    res.write('received upload:\n\n');
+//
+//    // This last line responds to the form submission with a list of the parsed data and files.
+//
+//    res.end(util.inspect({fields: fields, files: files}));
+//});
+//return;
